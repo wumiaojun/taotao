9a0c1d546513ebe4f277532db24b13d2f79f28e7
wu miaogong <miaogong.wu@ele.me>
2016-12-08 17:17:22 +0800

    修改logback.xml的文件位置不要放到默认的classpath下面，而是通过在web.xml中监听换路径

--- taotao-manager/taotao-web/src/main/java/com/taotao/controller/TbItemController.java
+++ taotao-manager/taotao-web/src/main/java/com/taotao/controller/TbItemController.java
@@ -2,11 +2,15 @@ package com.taotao.controller;
 
 import com.taotao.pojo.TbItem;
 import com.taotao.service.TbItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 /**
  * author: wumiaogong
@@ -16,12 +20,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/items")
 public class TbItemController {
 
+    final static Logger logger  =  LoggerFactory.getLogger(TbItemController.class );
     @Autowired
     private TbItemService tbItemService;
 
     @RequestMapping("/item/{item}")
     @ResponseBody
     public TbItem getTbItem(@PathVariable Long item){
+        System.out.println("范围======");
+               logger.debug( "现在的时间是 {}" ,  new Date().toString());
+
+                logger.info( " This time is {}" ,  new Date().toString());
+
+                logger.warn( " This time is {}" ,  new Date().toString());
+
+                logger.error( " This time is {}" ,  new Date().toString());
 
         System.out.println("进来了吗");
        return tbItemService.getTbitem(item);
